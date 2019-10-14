8fe97b23b51e303ad7846f12e4cd9e12599152f4
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-10-14 11:34:40 +0300

    Separare cod

--- html/ec2.js
+++ html/ec2.js
@@ -1,10 +1,12 @@
-document.getElementById("id_logic_version").innerHTML = "logic verions = 2019.10.14.0";
+
+document.getElementById("id_logic_version").innerHTML = "logic verions = 2019.10.14.1";
+document.getElementById("id_button").addEventListener("click", solve_eq2);
 
 function solve_eq2()
 {
-    var a = document.getElementById("id_a").Value;
-    var b = document.getElementById("id_b").Value;
-    var c = document.getElementById("id_c").Value;
+    var a = document.getElementById("id_a").value;
+    var b = document.getElementById("id_b").value;
+    var c = document.getElementById("id_c").value;
 
     var delta = b*b -4*a*c;
     var x1_im, x1_re, x2_im, x2_re;
