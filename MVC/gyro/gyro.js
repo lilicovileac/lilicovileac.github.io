a70f625f52c6f712e3db03ffc3e0e0a8dfb85564
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-11-04 12:39:56 +0200

    Trying to add code for drawing a circle

--- MVC/gyro/gyro.js
+++ MVC/gyro/gyro.js
@@ -1,13 +1,33 @@
-document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.3";
+document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.5";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
+
+function draw(angle_x, angle_y)
+{
+    // pbtinem context grafic
+    var canvas = document.getElementById("id_canvas");
+    var context = canvas.getContext("2d");
+    var raza = 10;
+    //contruim o cale
+    context.beginPath();
+        
+    //adaugam un cerc la cale
+    var x = angle_x / 90 * (canvas.width  /2  - raza) + canvas.width  / 2;
+    var y = angle_y / 90 * (canvas.height /2  - raza) + canvas.height / 2;
+    context.arc( x, y, r, 0, 2* Math.PI);
+        
+    //desenam calea
+    context.stroke();
+}
 
 function on_gyro_data_uab(e)
 {
     document.getElementById("id_alpha").innerHTML = e.alpha.toFixed(2);
     document.getElementById("id_beta").innerHTML = e.beta.toFixed(2);
     document.getElementById("id_gamma").innerHTML = e.gamma.toFixed(2);
+
+    draw(e.beta, e.gamma);
 }
 
 function on_acc_data_uab(e)
