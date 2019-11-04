9b765268fc742a31b5410937289384a898f7f0e8
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-11-04 10:48:14 +0200

    added toFixed to coordinates

--- MVC/gyro/gyro.js
+++ MVC/gyro/gyro.js
@@ -1,9 +1,9 @@
-document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.0";
+document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.1";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
 function on_gyro_data_uab(e)
 {
-    document.getElementById("id_alpha").innerHTML = e.alpha;
-    document.getElementById("id_beta").innerHTML = e.beta;
-    document.getElementById("id_gamma").innerHTML = e.gamma;
+    document.getElementById("id_alpha").innerHTML = e.alpha.toFixed(2);
+    document.getElementById("id_beta").innerHTML = e.beta.toFixed(2);
+    document.getElementById("id_gamma").innerHTML = e.gamma.toFixed(2);
 }
