b87aae233a5ec2037e087cbe5a144ccf2a036b17
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-11-04 11:44:23 +0200

    Added acceleration values in the program

--- MVC/gyro/gyro.js
+++ MVC/gyro/gyro.js
@@ -1,9 +1,20 @@
-document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.1";
+document.getElementById("id_logic_version").innerHTML = "Logic = 2019.11.4.2";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab);
+window.addEventListener("devicemotion", on_acc_data_uab);
+
 function on_gyro_data_uab(e)
 {
     document.getElementById("id_alpha").innerHTML = e.alpha.toFixed(2);
     document.getElementById("id_beta").innerHTML = e.beta.toFixed(2);
     document.getElementById("id_gamma").innerHTML = e.gamma.toFixed(2);
+}
+
+function on_acc_data_uab(e)
+{
+    var acc = e.accelerationIncludingGravity;
+
+    document.getElementById("id_acc_x").innerHTML = acc.x.toFixed(2);
+    document.getElementById("id_acc_y").innerHTML = acc.y.toFixed(2);
+    document.getElementById("id_acc_z").innerHTML = acc.z.toFixed(2);
 }
