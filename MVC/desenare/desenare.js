ae608fc64f9b883cddd633bcf4f7dad00a782763
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-11-25 13:45:11 +0200

    Made final changes to desenare

--- MVC/desenare/desenare.js
+++ MVC/desenare/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "2019.11.25.5";
+document.getElementById("id_logic").innerHTML = "2019.11.25.8";
 
 document.getElementById("id_start").addEventListener("click", start);
 document.getElementById("id_stop").addEventListener("click", stop);
@@ -6,6 +6,7 @@ document.getElementById("id_stop").addEventListener("click", stop);
 var timer_id;
 var unghi = {};
 unghi.valoare = 0;
+var muncitor = null;
 
 function desenare(unghi)
 {
@@ -39,11 +40,18 @@ function start()
 
     timer_id = setInterval(desenare, 20, unghi);
 
-    var muncitor = new Worker("prime.js");
-    muncitor.onmessage = function(e)
+    if(muncitor == null)
     {
-        document.getElementById("id_prime").innerHTML = e.data;
+        muncitor = new Worker("prime.js");
+        muncitor.onmessage = function(e)
+        {
+            document.getElementById("id_prime").innerHTML = e.data;
+        }
+        muncitor.postMessage("stop");
     }
+    else 
+        muncitor.postMessage("start");
+    
 }
 
 function stop()
@@ -52,4 +60,5 @@ function stop()
     document.getElementById("id_stop").disabled = true;
 
     clearInterval(timer_id);
+    muncitor.postMessage("stop");
 }
