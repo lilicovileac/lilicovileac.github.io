1713d0c691a7089f89a21ba876b012e7a4b39ce5
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-12-02 11:09:34 +0200

    New Touch app

---
+++ MVC/Touch/touch.js
@@ -0,0 +1,17 @@
+document.getElementById("id_version").innerHTML = "2019.12.02.0";
+
+window.addEventListener("touchstart", touch_start_uab);
+
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+
+function touch_start_uab(p)
+{
+    var touches = p.changedTouches;
+    for( var i = 0; i < touches.length; i++)
+    {
+        context.beginPath();
+        context.arc(t[i].pageX, t[i].pageY, 10, 0, 2* Math.PI);
+        context.stroke();
+    }
+}
