655abf4e5bb205a22bef7fe9c026bd822699dfa3
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-12-02 11:28:23 +0200

    Minor error checkings

--- MVC/Touch/touch.js
+++ MVC/Touch/touch.js
@@ -1,17 +1,36 @@
-document.getElementById("id_version").innerHTML = "2019.12.02.0";
+document.getElementById("id_version").innerHTML = "2019.12.02.1";
 
 window.addEventListener("touchstart", touch_start_uab);
+window.addEventListener("touchmove", touch_move_uab);
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
+var last_position = [];
+
 function touch_start_uab(p)
 {
-    var touches = p.changedTouches;
-    for( var i = 0; i < touches.length; i++)
+    var t = p.changedTouches;
+    for(var i = 0; i < t.length; i++)
     {
         context.beginPath();
-        context.arc(t[i].pageX, t[i].pageY, 10, 0, 2* Math.PI);
+        context.arc(t[i].pageX, t[i].pageY, 10, 0, 2 * Math.PI);
         context.stroke();
+
+        var touch_info = {};
+        touch_info.x = t[i].pageX;
+        touch_info.y = t[i].pageY;
+        touch_info.id = t[i].identifier;
+
+        last_position.push(touch_info);
+    }
+}
+
+function touch_move_uab(p)
+{
+    var t = p.changedTouches;
+    for(var i = 0; i < t.length; i++)
+    {
+        
     }
 }
