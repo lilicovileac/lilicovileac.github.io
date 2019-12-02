d544314951f29cd8b219b0009a549a601a68ba0b
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-12-02 11:56:39 +0200

    Adding files for line drawing

--- MVC/Touch/touch.js
+++ MVC/Touch/touch.js
@@ -8,19 +8,36 @@ var context = canvas.getContext("2d");
 
 var last_position = [];
 
+function get_random_color()
+{
+    var culoare = "#";
+    var caractere = "0123456789ABCDEF";
+    for(var i=0; i< 6; i++)
+    {
+        culoare += caractere[Math.floor(Math.random() * 16)];
+    }
+    return culoare;
+}
+
 function touch_start_uab(p)
 {
     var t = p.changedTouches;
     for(var i = 0; i < t.length; i++)
-    {
-        context.beginPath();
-        context.arc(t[i].pageX, t[i].pageY, 10, 0, 2 * Math.PI);
-        context.stroke();
-
-        var touch_info = {};
+    {   var touch_info = {};
         touch_info.x = t[i].pageX;
         touch_info.y = t[i].pageY;
         touch_info.id = t[i].identifier;
+        touch_info.color = get_random_color();
+
+        context.beginPath();
+        context.arc(t[i].pageX, t[i].pageY, 10, 0, 2 * Math.PI);
+        context.strokeStyle = touch_info.color;
+        context.fillStyle = touch_info.color;
+        context.lineWidth = 1;
+        context.fill();
+        context.stroke();
+
+        
 
         last_position.push(touch_info);
     }
@@ -31,6 +48,24 @@ function touch_move_uab(p)
     var t = p.changedTouches;
     for(var i = 0; i < t.length; i++)
     {
-        
+        var index_touch = -1;
+        for(var j = 0; j < last_position.length; j++ )
+        {
+            if (last_position[j].id == t[i].identifier)
+            {
+                index_touch = j;
+                break;
+            }
+        }
+
+        context.beginPath();
+        context.moveTo(last_position[index_touch].x, last_position[index_touch].y);
+        context.lineTo(t[i].pageX, t[i].pageY);
+        context.strokeStyle =last_position[index_touch].color;
+        context.fillStyle = last_position[index_touch].color;
+        context.lineWidth = 20;
+        context.fill();
+        context.stroke();
+
     }
 }
