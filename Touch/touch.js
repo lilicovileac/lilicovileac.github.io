800f8239930c8e9bc68e8bc4939b8c66b50bf75c
lilicovileac <30460044+lilicovileac@users.noreply.github.com>
2019-12-02 12:27:08 +0200

    Added corect position using Canvas.getbounderies..

--- Touch/touch.js
+++ Touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_version").innerHTML = "2019.12.02.2";
+document.getElementById("id_version").innerHTML = "2019.12.02.3";
 
 window.addEventListener("touchstart", touch_start_uab);
 window.addEventListener("touchmove", touch_move_uab);
@@ -6,6 +6,8 @@ window.addEventListener("touchend", touch_end_uab);
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
+
+var canvas_rect = canvas.getBoundingClientRect();
 
 var last_position = [];
 
@@ -31,7 +33,7 @@ function touch_start_uab(p)
         touch_info.color = get_random_color();
 
         context.beginPath();
-        context.arc(t[i].pageX, t[i].pageY, 10, 0, 2 * Math.PI);
+        context.arc(t[i].pageX-canvas_rect.left, t[i].pageY - canvas_rect.top , 10, 0, 2 * Math.PI);
         context.strokeStyle = touch_info.color;
         context.fillStyle = touch_info.color;
         context.lineWidth = 1;
@@ -60,13 +62,16 @@ function touch_move_uab(p)
         }
 
         context.beginPath();
-        context.moveTo(last_position[index_touch].x, last_position[index_touch].y);
-        context.lineTo(t[i].pageX, t[i].pageY);
+        context.moveTo(last_position[index_touch].x - canvas_rect.left, last_position[index_touch].y - canvas_rect.top);
+        context.lineTo(t[i].pageX - canvas_rect.left, t[i].pageY - canvas_rect.top);
         context.strokeStyle =last_position[index_touch].color;
         context.fillStyle = last_position[index_touch].color;
         context.lineWidth = 20;
         context.fill();
         context.stroke();
+
+        last_position[index_touch].x = t[i].pageX;
+        last_position[index_touch].y = t[i].pageY;
 
     }
 }
